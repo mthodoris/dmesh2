3a209f4aabeb2eb1bbc7a3fc10226a473b907da5
mthodoris <thodorisman@gmail.com>
2026-08-26 13:30:42 +0300

    reduce knn spatial k

--- exp/utils/common.py
+++ exp/utils/common.py
@@ -1,6 +1,6 @@
 DMIN_K = 100                        # hyperparameter for differentiable min function
 INIT_PREAL_THRESH = 0.01            # points with preal less than this threshold will be considered to have 0 preal
 PPOS_SIGMOID_MAX_INPUT = 32.0       # maximum input of the sigmoid function
-QFACE_KNN_SPATIAL_K = 20            # number of nearest neighbors to consider in finding the query simplexes
+QFACE_KNN_SPATIAL_K = 10            # number of nearest neighbors to consider in finding the query simplexes
 PROB_THRESH = 1e-3                  # minimum probability to be considered, simplexes with lower probability will be ignored
 MIN_LR = 1e-6                       # minimum learning rate
